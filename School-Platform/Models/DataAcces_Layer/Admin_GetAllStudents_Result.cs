a20e4498f192ba1a6c78bb02d7e4c36df0354552
LucianSchipor <schiporlucian20021@gmail.com>
2023-05-22 00:17:27 +0300

    [API] - GetAllStudents stored procedure works fine

--- School-Platform/Models/DataAcces_Layer/Admin_GetAllStudents_Result.cs
+++ School-Platform/Models/DataAcces_Layer/Admin_GetAllStudents_Result.cs
@@ -9,6 +9,9 @@ namespace School_Platform.Models.DataAcces_Layer
     public class Admin_GetAllStudents_Result
     {
         public int User_ID { get; set; }
+        public string Username { get; set; }
         public string Name { get; set; }
+        public int Class_ID { get; set; }
+        public int AbsencesCount { get; set; }
     }
 }
